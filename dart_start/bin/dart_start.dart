ae600d085f9bbec44d9d0877cc82c0d02b4f59de
ovchie <fast.rem1ck3@gmail.com>
2021-06-21 22:53:43 +0300

    var types

--- dart_start/bin/dart_start.dart
+++ dart_start/bin/dart_start.dart
@@ -1,3 +1,45 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main() {
+  print('double:');
+  print('~~~~~');
+  double my_weight = 71.5;
+  print(my_weight);
+  print(my_weight.runtimeType);
+  print('~~~~~\n');
+
+  print('bool:');
+  print('~~~~~');
+  bool is_adult = true;
+  print(is_adult);
+  print(is_adult.runtimeType);
+  print('~~~~~\n');
+
+  print('String:');
+  print('~~~~~');
+  String my_name = 'Sergey';
+  print(my_name);
+  String my_surname = "Ovchhinnikov";
+  print(my_surname);
+  String fio = '''
+  Sergey
+  Ovchinnikov
+  Alexsandrovich
+  ''';
+  print(fio);
+  String my_personinfo =
+      "Name: ${my_name.toUpperCase()}, weight: $my_weight kg";
+  print(my_personinfo);
+  print('~~~~~\n');
+
+  print('Runes:');
+  print('~~~~~');
+  Runes hello = Runes('\u1f605');
+  print(hello);
+  print(String.fromCharCodes(hello));
+  print('~~~~~\n');
+
+  print('Symbol:');
+  print('~~~~~');
+  Symbol s = #f;
+  print(s);
+  print('~~~~~\n');
 }
